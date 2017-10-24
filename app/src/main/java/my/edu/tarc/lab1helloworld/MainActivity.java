eac9aecbb57f8b03cef825f969aa960837c01c24
User <fcjun93@gmail.com>
2017-10-24 12:32:46 +0800

    Initial

---
+++ app/src/main/java/my/edu/tarc/lab1helloworld/MainActivity.java
@@ -0,0 +1,43 @@
+package my.edu.tarc.lab1helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //Global or module level variable
+    private EditText editTextName, editTextAge;
+    private TextView textViewMessage;
+
+    //onCreate = main() function
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Display the UI
+        //R = resources (res)
+        setContentView(R.layout.activity_main);
+
+        //Linking UI to program
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAge = (EditText) findViewById(R.id.editTextAge);
+        textViewMessage = (TextView) findViewById(R.id.textViewMessage);
+    }
+
+    public void DisplayMessage(View view){
+        String name;
+        Integer age;
+        name = editTextName.getText().toString();
+        age = Integer.parseInt(editTextAge.getText().toString());
+        age++;
+        textViewMessage.setText("Hi " +name +". You will be " +age + " old in 2018.");
+
+    }
+
+
+
+
+}
